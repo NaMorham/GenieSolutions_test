611f94031fd471c92bf4f35337abf95c7cb5ffcc
NaMorham <namorham@gmail.com>
2017-03-11 15:01:21 +1000

    GS_golang001 - Tokenise command string

--- golang/test/test4.go
+++ golang/test/test4.go
@@ -38,21 +38,39 @@ func main() {
         if str == "QUIT" {
             fmt.Println("Exiting...")
             break
-        } else if str == "MOVEX" {
-            if pt.x >= 5 {
-                pt.x = 0
-            } else {
-                pt.moveX(1)
-            }
-        } else if str == "MOVEY" {
-            if pt.y >= 5 {
-                pt.y = 0
-            } else {
-                pt.moveY(1)
-            }
-        } else {
-            fmt.Printf("Read invalid: [%q]\n",str)
+        }
+        tokens := strings.Split(str, " ")
+        numTokens := len(tokens)
+        fmt.Printf("tokens --> len() == [%d] %q\n", len(tokens), tokens)
+
+        switch (numTokens) {
+        case 0:
+            // no args, nothing to do
             continue
+        case 1:
+            // Commands with no arguments
+            if tokens[0] == "MOVE" {
+                fmt.Println("MOVE command not implemented")
+            } else if str == "MOVEX" {
+                if pt.x >= 5 {
+                    pt.x = 0
+                } else {
+                    pt.moveX(1)
+                }
+            } else if str == "MOVEY" {
+                if pt.y >= 5 {
+                    pt.y = 0
+                } else {
+                    pt.moveY(1)
+                }
+            }
+        case 3:
+            // Commands with 2 arguments
+            if tokens[0] == "PLACE" {
+                fmt.Println("PLACE command not implemented")
+            }
+        default:
+            fmt.Println("Unknown command [%s] %q", tokens[0], tokens[1:])
         }
         fmt.Printf("pt = [%v]\n", pt)
     }
